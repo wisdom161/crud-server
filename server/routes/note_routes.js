42e7aa93b3fe78ea97fd146118ae0c653419b5d4
Wisdom Liu <wisdom.code161@gmail.com>
2019-05-29 17:40:56 -0700

    created routes

--- server/routes/note_routes.js
+++ server/routes/note_routes.js
@@ -1,8 +1,56 @@
 //this will be exported into the master route file
+var ObjectID = require('mongodb').ObjectID;
+//mongo can't accept a string for ID and needs an object ID
+
 module.exports = function(app, db) {
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id //parameters in the URL
+    const details = {'_id': new ObjectID(id)}; //id goes here
+    db.db().collection('notes').findOne(details, (err, item) => {
+      if(err) {
+        res.send({'error': 'An error has occured'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+    console.log(req.body)
+    const id = req.params.id;
+    const details = {'_id': new ObjectID(id)};
+    db.db().collection('notes').deleteOne(details, (err, item) => {
+      if(err) {
+        res.send({'error': 'An Error has occured'})
+      } else {
+        res.send('Note ' + id + ' was deleted!')
+      }
+    })
+  })
+
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = {'_id': new ObjectID(id)};
+    const note = { text: req.body.body, title: req.body.title };
+    db.db().collection('notes').update(details, note, (err, item) => {
+      if(err) {
+        res.send({'error': 'An error has occured'});
+      } else {
+        res.send(item)
+      }
+    })
+  })
+
   app.post('/notes', (req, res) => {
     //we create the note here
-    console.log(req.body)
-    res.send('Hello')
+    const note = { text: req.body.body, title: req.body.title };
+    //make a .db() call on your db variable before calling .collection() 
+    db.db().collection('notes').insert(note, (err, results) => {
+      if(err) {
+        res.send({'error': 'An error has occured'})
+      } else {
+        res.send(results.ops[0])
+      }
+    })
   });
-}
+};
--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require ('body-parser');
+const db = require('./db/config');
 
 const app = express();
 const port = 8000;
 app.use(bodyParser.urlencoded({ extended: true}))
-require('./routes/master')(app, {});
-app.use(bodyParser)
-app.listen(port, () => {
-  console.log('We are live on ', port);
-})
 
+
+
+MongoClient.connect(db.url, { useNewUrlParser: true }, (err, database) => {
+  if (err) return console.log(err);
+  require('./routes/master')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ', port);
+  })
+})
